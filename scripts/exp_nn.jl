1ea25dacce77980409fc3fe52021e4e82117e06e
Serena Chan <serenaktc@gmail.com>
2025-09-25 23:22:19 -0400

    fixed exp nn, need metrics still

--- scripts/exp_nn.jl
+++ scripts/exp_nn.jl
@@ -2,7 +2,7 @@ using Pkg
 Pkg.activate("/home/golem/scratch/chans/lincs")
 
 using LincsProject, DataFrames, CSV, Dates, JSON, StatsBase, JLD2, SparseArrays, Dates, Printf, Profile
-using Flux, Random, OneHotArrays, CategoricalArrays, ProgressBars, CUDA, Statistics, CairoMakie, LinearAlgebra
+using Flux, Random, OneHotArrays, CategoricalArrays, ProgressBars, CUDA, Statistics, CairoMakie, LinearAlgebra, MLUtils
 
 CUDA.device!(0)
 # GC.gc()
@@ -13,29 +13,54 @@ CUDA.device!(0)
 #######################################################################################################################################
 
 # untrt only
-const data_path = "data/lincs_untrt_data.jld2"
-const save_path_base = "untrt"
+# const data_path = "data/lincs_untrt_data.jld2"
+# const save_path_base = "untrt"
 
 # untrt and trt
-# const data_path = "data/lincs_trt_untrt_data.jld2"
-# const save_path_base = "trt"
+const data_path = "data/lincs_trt_untrt_data.jld2"
+const save_path_base = "trt"
 
 # params
-const batch_size = 64
-const n_epochs = 1
+const batch_size = 512
+const n_epochs = 30
 const embed_dim = 128
 const hidden_dim = 256
 const mid_dim = 64
 const latent_dim = 32
-const mask_ratio = 0.1
-const mask_value = 0.0
+const mask_ratio = 0.1f0
+const mask_value = 0.0f0
 const lr = 0.001
 # const drop_prob = 0.05
 
 # notes
 const gpu_info = "this was on kraken"
-const dataset_note = "untrt"
-const additional_notes = "demo run"
+const dataset_note = "trt"
+const additional_notes = "trying with trt and untrt data, 30ep"
+
+# # untrt only
+# data_path = "data/lincs_untrt_data.jld2"
+# save_path_base = "untrt"
+
+# # untrt and trt
+# # data_path = "data/lincs_trt_untrt_data.jld2"
+# # save_path_base = "trt"
+
+# # params
+# constbatch_size = 64
+# n_epochs = 10
+# embed_dim = 128
+# hidden_dim = 256
+# mid_dim = 64
+# latent_dim = 32
+# mask_ratio = 0.1f0
+# mask_value = 0.0f0
+# lr = 0.001
+# # drop_prob = 0.05
+
+# # notes
+# gpu_info = "this was on kraken"
+# dataset_note = "untrt"
+# additional_notes = "demo run"
 
 #######################################################################################################################################
 ### DATA
@@ -87,6 +112,11 @@ for now, will use:
         - forces it to get good at the task at hand
         - also less computational effort to caluclate loss on more values?
 mostly just becasue this is closer to the transformer task and this is the main goal; comparison!
+
+OR:
+- since we only calculate loss on the 10% masked values
+- the model has no incentive to maintain meaningful representations for the 90% unmasked values
+- therefore this leads to poor overall representations...?
 =#
 
 # for use of both CPU and GPU
@@ -101,19 +131,30 @@ struct Mask
     mask_value::Float32
 end
 
+# function (m::Mask)(x::AbstractMatrix{Float32})
+#     X_masked = copy(x)
+#     mask_labels = fill(NaN32, size(x)) #!# NaN to ignore positions in the loss calculation
+
+#     for j in 1:size(x,2) # per column
+#         num_masked = ceil(Int, size(x,1) * m.mask_ratio)
+#         mask_positions = randperm(size(x,1))[1:num_masked]
+
+#         for pos in mask_positions
+#             mask_labels[pos, j] = x[pos, j] 
+#             X_masked[pos, j] = m.mask_value
+#         end
+#     end
+#     return X_masked, mask_labels
+# end
+
 function (m::Mask)(x::AbstractMatrix{Float32})
-    X_masked = copy(x)
-    mask_labels = fill(NaN32, size(x)) #!# NaN to ignore positions in the loss calculation
+    # random boolean mask for entire matrix for what to mask
+    mask_indices = rand_like(x) .< m.mask_ratio
 
-    for j in 1:size(x,2) # per column
-        num_masked = ceil(Int, size(x,1) * m.mask_ratio)
-        mask_positions = randperm(size(x,1))[1:num_masked]
+    # place mask on data
+    X_masked = ifelse.(mask_indices, m.mask_value, x)
+    mask_labels = ifelse.(mask_indices, x, NaN32)
 
-        for pos in mask_positions
-            mask_labels[pos, j] = x[pos, j] 
-            X_masked[pos, j] = m.mask_value
-        end
-    end
     return X_masked, mask_labels
 end
 
@@ -142,7 +183,7 @@ function Encoder(
     return Encoder(noise, compress)
 end
 
-Flux.@functor Encoder
+Flux.@functor Encoder (compress,)
 
 function (enc::Encoder)(input::Float32Matrix2DType)
     noised, labels = enc.noise(input)
@@ -182,6 +223,7 @@ struct Model
     mlp::Flux.Chain
     encoder::Encoder
     decoder::Decoder
+    # mlp_head::Flux.Chain
 end
 
 function Model(;
@@ -197,9 +239,10 @@ function Model(;
     )
 
     mlp = Flux.Chain(
-        Dense(num_genes => hidden_dim, relu),
-        # layernorm?
-        # dropout?
+        Dense(num_genes => hidden_dim),
+        LayerNorm(hidden_dim),
+        relu,
+        # Dropout(0.1),
         Dense(hidden_dim => embed_dim) # softplus?
     )
 
@@ -207,16 +250,20 @@ function Model(;
 
     decoder = Decoder(embed_dim, mid_dim, latent_dim)
 
+    # mlp_head = Flux.Chain(
+    #     Dense(embed_dim => num_genes)
+    # )
+
     return Model(mlp, encoder, decoder)
 end
 
 Flux.@functor Model
 
-function (model::Model)(x::AbstractMatrix{Float32})
-    embedding = model.mlp(x)
+function (model::Model)(input::AbstractMatrix{Float32})
+    embedding = model.mlp(input)
     latent, labels = model.encoder(embedding)
-    reconstructed = model.decoder(latent)
-    return reconstructed, labels
+    recon_embed = model.decoder(latent)
+    return recon_embed, labels
 end
 
 #######################################################################################################################################
@@ -249,6 +296,15 @@ end
 
 X_train, X_test = split_data(X, 0.2)
 
+# X_train_raw, X_test_raw = split_data(X, 0.2)
+
+# train_mean = mean(X_train_raw, dims=2)
+# train_std = std(X_train_raw, dims=2)
+# epsilon = 1e-8
+
+# X_train = (X_train_raw .- train_mean) ./ (train_std .+ epsilon)
+# X_test = (X_test_raw .- train_mean) ./ (train_std .+ epsilon)
+
 #######################################################################################################################################
 ### TRAINING
 #######################################################################################################################################
@@ -265,6 +321,132 @@ model = Model(
 
 opt = Flux.setup(Adam(lr), model)
 
-function loss(model::Model, x, y)
-    preds = model(x)
+function loss(model::Model, x, mode::String)
+    preds, trues = model(x)
+    preds_flat = vec(preds)
+    trues_flat = vec(trues)
+
+    mask = .!isnan.(trues_flat)
+    
+    preds_masked = preds_flat[mask]
+    trues_masked = trues_flat[mask]
+    
+    error = Flux.mse(preds_masked, trues_masked)
+
+    if mode == "train"
+        return error
+    end
+    if mode == "test"
+        return error, preds_masked, trues_masked
+    end
+end
+
+train_losses = Float32[]
+test_losses = Float32[]
+all_preds = Float32[]
+all_trues = Float32[]
+
+for epoch in ProgressBar(1:n_epochs)
+    train_epoch_losses = Float32[]
+    for start_idx in 1:batch_size:size(X_train, 2)
+        end_idx = min(start_idx + batch_size - 1, size(X_train, 2))
+        x_gpu = gpu(X_train[:, start_idx:end_idx])
+
+        loss_val, grads = Flux.withgradient(model) do m
+            loss(m, x_gpu, "train")
+        end
+        Flux.update!(opt, model, grads[1])
+        loss_val = loss(model, x_gpu, "train")
+        push!(train_epoch_losses, loss_val)
+    end
+    push!(train_losses, mean(train_epoch_losses))
+
+    test_epoch_losses = Float32[]
+    for start_idx in 1:batch_size:size(X_test, 2)
+        end_idx = min(start_idx + batch_size - 1, size(X_test, 2))
+        x_gpu = gpu(X_test[:, start_idx:end_idx])
+
+        test_loss_val, preds_masked, trues_masked = loss(model, x_gpu, "test")
+        push!(test_epoch_losses, test_loss_val)
+
+        if epoch == n_epochs
+            append!(all_preds, cpu(preds_masked))
+            append!(all_trues, cpu(trues_masked))
+        end
+    end
+    push!(test_losses, mean(test_epoch_losses))
+end
+
+correlation = cor(all_trues, all_preds)
+
+
+#######################################################################################################################################
+### EVAL/PLOT
+#######################################################################################################################################
+
+# mk dir
+timestamp = Dates.format(now(), "yyyy-mm-dd_HH-MM")
+save_dir = joinpath("plots", save_path_base, "exp_nn", timestamp)
+mkpath(save_dir)
+
+# loss plot
+fig_loss = Figure(size = (800, 600))
+ax_loss = Axis(fig_loss[1, 1], 
+    xlabel="epoch", 
+    ylabel="loss (mse)", 
+    title="train vs. test loss"
+)
+lines!(ax_loss, 1:n_epochs, train_losses, label="train loss", linewidth=2)
+lines!(ax_loss, 1:n_epochs, test_losses, label="test loss", linewidth=2)
+axislegend(ax_loss, position=:rt)
+display(fig_loss)
+save(joinpath(save_dir, "loss.png"), fig_loss)
+
+#######################################################################################################################################
+### LOG
+#######################################################################################################################################
+
+# save model!!!
+model_cpu = cpu(model)
+jldsave(joinpath(save_dir, "model_object.jld2"); model=model_cpu) # whole model
+jldsave(joinpath(save_dir, "model_state.jld2"); model_state=Flux.state(model_cpu)) # need to recreate model + apply state to load back in
+
+jldsave(joinpath(save_dir, "losses.jld2"); 
+    epochs = 1:n_epochs, 
+    train_losses = train_losses, 
+    test_losses = test_losses
+)
+jldsave(joinpath(save_dir, "predstrues.jld2"); 
+    all_preds = all_preds, 
+    all_trues = all_trues
+    # all_gene_indices = all_gene_indices
+)
+jldsave(joinpath(save_dir, "test_data.jld2"); X=X_test)
+
+end_time = now()
+run_time = end_time - start_time
+total_minutes = div(run_time.value, 60000)
+run_hours = div(total_minutes, 60)
+run_minutes = rem(total_minutes, 60)
+
+params_txt = joinpath(save_dir, "params.txt")
+open(params_txt, "w") do io
+    println(io, "PARAMETERS:")
+    println(io, "########### $(gpu_info)")
+    println(io, "dataset = $(dataset_note)")
+    println(io, "masking_ratio = $mask_ratio")
+    println(io, "mask_value = $mask_value")
+    println(io, "batch_size = $batch_size")
+    println(io, "n_epochs = $n_epochs")
+    println(io, "embed_dim = $embed_dim")
+    println(io, "hidden_dim = $hidden_dim")
+    println(io, "mid_dim = $mid_dim")
+    println(io, "latent_dim = $latent_dim")
+    println(io, "learning_rate = $lr")
+    # println(io, "dropout_probability = $drop_prob")
+    println(io, "ADDITIONAL NOTES: $(additional_notes)")
+    println(io, "run_time = $(run_hours) hours and $(run_minutes) minutes")
+    println(io, "correlation = $correlation")
+    # println(io, "mse model = $mse_model")
+    # println(io, "mse baseline = $mse_baseline")
 end
